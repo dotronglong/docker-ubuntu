a89fbea0f278075adb50b2e68a92723aa3c6e886
Long Do <me@dotronglong.com>
2017-04-16 16:15:30 +0700

    Init master

---
+++ README.md
@@ -0,0 +1,6 @@
+# docker-ubuntu
+- A vagrant box is based on Ubuntu 12.04 LTS with Docker and Docker Compose are installed
+- In this box
+  + Ubuntu 12.04 LTS
+  + Docker CE
+  + Docker Compose 1.12.0
---
+++ provision.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+apt-get update
+apt-get install \
+  apt-transport-https \
+  ca-certificates \
+  curl git vim \
+  software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+apt-key fingerprint 0EBFCD88
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+apt-get install docker-ce
+
+apt-get install \
+    linux-image-extra-$(uname -r) \
+    linux-image-extra-virtual
+
+curl -L "https://github.com/docker/compose/releases/download/1.12.0/docker-compose-$(uname -s)-$(uname -m)" \
+     -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
